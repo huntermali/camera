9fd346f894bc29a9bc534e4c3c991c2afd97a6b5
Hunter Malinowski <huntermalinowski@gmail.com>
2020-08-07 12:07:19 -0700

    Initial commit

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func selfieTaker(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
